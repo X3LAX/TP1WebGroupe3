c7571914b0a36da41b7f439c532b39b10259736d
boretti alexander <alexander.boretti@etu.unice.fr>
2025-01-20 12:13:24 +0100

    pas mal les bottes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("lié avec succes!");
+const buttonajouterpanier1 = document.getElementById('btn1');
+console.log(buttonajouterpanier1);
+
+buttonajouterpanier1.addEventListener("click", () => { 
+    alert('Bottes ajouté au panier, YUPPPI')
+})
+
+const buttonajouterpanier2 = document.getElementById('btn2');
+console.log(buttonajouterpanier2);
+
+buttonajouterpanier2.addEventListener("click", () => {
+    alert('Bottes ajouté au panier, YUPPPI')
+})
+
+const buttonajouterpanier3 = document.getElementById('btn3');
+console.log(buttonajouterpanier3);
+
+buttonajouterpanier3.addEventListener("click", () => {
+    alert('Bottes ajouté au panier, YUPPPI')
+})
